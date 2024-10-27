9ba696d1e46d8e403ef6accc79181f9971e9031a
al070698 <145055756+al070698@users.noreply.github.com>
2024-10-26 19:09:05 -0600

    Delete Práctica Patrón de Diseño Singleton directory

--- Práctica Patrón de Diseño Singleton/SingletonEnum.java
+++
@@ -1,9 +0,0 @@
-package patrondediseñosingleton;
-
-public enum SingletonEnum {
-    INSTANCIA;
-
-    public void mostrarNombreClase() {
-        System.out.println("Esta clase se llama: " + this.getClass().getSimpleName());
-    }
-}
