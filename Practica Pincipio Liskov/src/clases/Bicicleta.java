6a0be0e7b3b652817e8bcbcf242b40101778e336
al070698 <145055756+al070698@users.noreply.github.com>
2024-11-15 08:45:09 -0600

    Create Bicicleta.java

---
+++ Practica Pincipio Liskov/src/clases/Bicicleta.java
@@ -0,0 +1,19 @@
+package clases;
+
+class Bicicleta extends Vehiculo {
+    
+    public Bicicleta() {
+        this.kilometrosRecorridos = 0;
+    }
+
+    @Override
+    public void mover(double distancia) {
+        kilometrosRecorridos += distancia;
+        System.out.println("La bicicleta ha recorrido " + distancia + " km.");
+    }
+
+    @Override
+    public double calcularAutonomia() {
+       return -1; //La bicicleta no tiene autonomia de combustible
+    }   
+}
